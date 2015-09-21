d06b81c9d092ae66dfca7a4dc2a926bb8ef0d348
yurun <miao.work2010@gmail.com>
2015-09-21 15:36:25 +0800

    dev

--- examples/basic/spark_app_accumulator.py
+++ examples/basic/spark_app_accumulator.py
@@ -15,7 +15,7 @@ source = sc.parallelize(["row1_col1row1_col2 row1_col3", "row1_col1row1_col2 row
                          "row2_col1 row2_col2row3_col3", "row3_col1 row3_col2 row3_col3"])
 
 
-def filter(columns):
+def lineFilter(columns):
     allLines.add(1)
 
     if columns and len(columns) == 3:
@@ -27,7 +27,7 @@ def filter(columns):
 
         return False
 
-columns = source.map(lambda line: line.split(" ")).filter(filter)
+columns = source.map(lambda line: line.split(" ")).filter(lineFilter)
 
 rows = columns.map(
     lambda columns: Row(col1=columns[0], col2=columns[1], col3=columns[2]))
@@ -38,6 +38,8 @@ table.registerAsTable("temp_mytable")
 
 datas = hc.sql("select * from temp_mytable").collect()
 
+sc.stop()
+
 if datas:
     for data in datas:
         print data
@@ -45,5 +47,3 @@ if datas:
 print "allLines:", allLines.value
 print "successLines:", successLines.value
 print "errorLines: ", errorLines.value
-
-sc.stop()
