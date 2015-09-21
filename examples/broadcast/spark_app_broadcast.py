8e2ee7af35934afd122d27beb94f300411d8df48
yurun <miao.work2010@gmail.com>
2015-09-21 19:22:52 +0800

    dev

--- examples/broadcast/spark_app_broadcast.py
+++ examples/broadcast/spark_app_broadcast.py
@@ -4,6 +4,17 @@ from pyspark import SparkConf, SparkContext
 
 conf = SparkConf().setAppName("spark_app_broadcast")
 
-sc = SparkContext(conf = conf)
+sc = SparkContext(conf=conf)
+
+maps = {"key1": "val1", "key2": "val2", "key3": "other"}
+
+brMaps = sc.broadcast(maps)
+
+keys = sc.parallelize(["key1", "key2", "key3"])
+
+vals = source.map(lambda e: brMaps[e]).collect()
 
 sc.stop()
+
+for val in vals:
+    print val
