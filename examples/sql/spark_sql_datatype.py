6462bfcc345fe32b6caf07e4aae23a928b3f2169
yurun <miao.work2010@gmail.com>
2015-09-22 10:50:27 +0800

    dev

--- examples/sql/spark_sql_datatype.py
+++ examples/sql/spark_sql_datatype.py
@@ -14,7 +14,7 @@ source = sc.parallelize([(int("127"), int("32767"), int("2147483647"), long(
     "9223372036854775807"), float("1.1"), float("2.2"), Decimal("3.3"), "str", bool(0), datetime(2015, 9, 22, 9, 39, 45), date(2015, 9, 22), [1, 2, 3], {"key": "value"})])
 
 schema = StructType([StructField("byte", ByteType(), False), StructField("short", ShortType(), False), StructField(
-    "int", IntegerType(), False), StructField("long", LongType(), False), StructField("float", FloatType(), False), StructField("double", DoubleType(), False), StructField("decimal", DecimalType(), False), StructField("string", StringType(), False), StructField("boolean", BooleanType(), False), StructField("timestamp", TimestampType(), False), StructField("date", DateType(), False), StructField("array", ArrayType(IntegerType(), False), False), StructField("map", MapType(StringType(), StringType(), False))])
+    "int", IntegerType(), False), StructField("long", LongType(), False), StructField("float", FloatType(), False), StructField("double", DoubleType(), False), StructField("decimal", DecimalType(), False), StructField("string", StringType(), False), StructField("boolean", BooleanType(), False), StructField("timestamp", TimestampType(), False), StructField("date", DateType(), False), StructField("array", ArrayType(IntegerType(), False), False), StructField("map", MapType(StringType(), StringType(), False), False)])
 
 table = hc.applySchema(source, schema)
 
