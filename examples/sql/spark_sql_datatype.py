8a69113060466717c00d9bcb87619d8bbbde890a
yurun <miao.work2010@gmail.com>
2015-09-22 09:53:41 +0800

    dev

--- examples/sql/spark_sql_datatype.py
+++ examples/sql/spark_sql_datatype.py
@@ -14,7 +14,7 @@ source = sc.parallelize([(int("127"), int("32767"), int("2147483647"), long(
     "9223372036854775807"), float("1.1"), float("2.2"), Decimal("3.3"), "str", bool(0), datetime(2015, 9, 22, 9, 39, 45), date(2015, 9, 22))])
 
 schema = StructType([StructField("byte", ByteType(), False), StructField("short", ShortType(), False), StructField(
-    "int", IntegerType, False), StructField("long", LongType(), False), StructField("float", FloatType(), False), StructField("double", DoubleType(), False), StructField("decimal", DecimalType(), False), StructField("string", StringType(), False), StructField("timestamp", TimestampType(), False), StructField("date", DateType(), False)])
+    "int", IntegerType, False), StructField("long", LongType(), False), StructField("float", FloatType(), False), StructField("double", DoubleType(), False), StructField("decimal", DecimalType(), False), StructField("boolean", BooleanType(), False), StructField("string", StringType(), False), StructField("timestamp", TimestampType(), False), StructField("date", DateType(), False)])
 
 hc.applySchema(source, schema)
 
