8a4faff353fb43f536c2614e9b82b3f988c6e10a
yurun <miao.work2010@gmail.com>
2015-09-22 10:23:24 +0800

    dev

--- examples/sql/spark_sql_datatype.py
+++ examples/sql/spark_sql_datatype.py
@@ -20,7 +20,8 @@ table = hc.applySchema(source, schema)
 
 table.registerAsTable("temp_table")
 
-rows = hc.sql("select * from temp_table").collect()
+rows = hc.sql(
+    "select byte,short,int,long,float,double,decimal,string,boolean,timestamp,date,array,map.key,struct.first,struct.second,struct.third from temp_table").collect()
 
 sc.stop()
 
