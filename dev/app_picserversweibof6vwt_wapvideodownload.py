e82c2fb90ddc97d594682ba8cbd2f666c7764787
yurun <miao.work2010@gmail.com>
2015-09-23 15:33:55 +0800

    dev

--- dev/app_picserversweibof6vwt_wapvideodownload.py
+++ dev/app_picserversweibof6vwt_wapvideodownload.py
@@ -230,9 +230,7 @@ result = hc.sql("""
 select date,province,isp,cdn,idc,ua,version,video_network, video_error_code,video_error_msg,video_play_type,
         init_timetag,cal_buffer_num.buffer_count,cal_buffer_num.buffer_smaller_500ms_count,cal_buffer_num.buffer_bigger_2min_count,
         play_process_group,sum(video_play_type_duration) as sum_video_play_type_duration,
-        sum(cal_buffer_num.buffer_t_sum) as sum_buffer_t_sum,
-        sum(video_play_duration) as sum_video_play_duration,sum(video_duration) as sum_video_duration,
-        sum(play_process) as sum_play_process
+        sum(cal_buffer_num.buffer_t_sum) as sum_buffer_t_sum
         from temp_table2
         group by date,province,isp,cdn,idc,ua,version,video_network, video_error_code,video_error_msg,video_play_type,
         init_timetag,cal_buffer_num.buffer_count,cal_buffer_num.buffer_smaller_500ms_count,cal_buffer_num.buffer_bigger_2min_count,
