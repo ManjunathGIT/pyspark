58546df807801e265444c02ab0ff433a4e2d9b63
yurun <miao.work2010@gmail.com>
2015-09-23 15:56:58 +0800

    dev

---
+++ examples/sql/xinqi.py
@@ -0,0 +1,31 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import HiveContext, StructType, StructField, StringType
+
+conf = SparkConf().setAppName("spark_sql_delimiter_specify_schema")
+
+sc = SparkContext(conf=conf)
+
+hc = HiveContext(sc)
+
+source = sc.parallelize(["  "])
+
+columns = source.map(lambda line: line.split(" ")).filter(
+    lambda columns: columns and len(columns) == 3)
+
+rows = columns.map(
+    lambda columns: (columns[0], columns[1], columns[2]))
+
+schema = StructType([StructField("col1", StringType(), False), StructField(
+    "col2", StringType(), False), StructField("col3", StringType(), False)])
+
+table = hc.applySchema(rows, schema)
+
+table.registerAsTable("temp_mytable")
+
+datas = hc.sql("select sum(col1) from temp_mytable").collect()
+
+sc.stop()
+
+if datas:
+    for data in datas:
+        print data
