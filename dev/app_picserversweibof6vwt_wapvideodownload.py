08992240ab0a256cd1e58125d12865b4b0e8475d
yurun <miao.work2010@gmail.com>
2015-09-23 10:34:31 +0800

    dev

--- dev/app_picserversweibof6vwt_wapvideodownload.py
+++ dev/app_picserversweibof6vwt_wapvideodownload.py
@@ -40,48 +40,28 @@ def lineParse(line):
         version = ua.split("__")[2]
 
         if version == "5.4.0" or version == "5.4.5" or version == "5.4.5_beta":
-            return videodownload_info
-
-        if "__date" not in jsonObj or "video_mediaid" not in jsonObj or "video_url" not in jsonObj or "ua" not in jsonObj or "video_cdn" not in jsonObj or "video_network" not in jsonObj or "ip" not in jsonObj or "video_play_type" not in jsonObj or "video_play_type_duration" not in jsonObj or "video_error_code" not in jsonObj or "video_error_msg" not in jsonObj or "buffer_duration_list" not in jsonObj or "video_duration" not in jsonObj or "video_play_duration" not in jsonObj:
-            return None
-
-        __date = None
-        video_mediaid = None
-        video_url = None
-        ua = None
-        video_cdn = None
-        video_network = None
-        ip = None
-        video_play_type = None
-        video_play_type_duration = None
-        video_error_code = None
-        video_error_msg = None
-        buffer_duration_list = None
-        video_duration = None
-        video_play_duration = None
-
-        ua = jsonObj["ua"]
-
-        """
-        if len(ua.split("__")) < 3:
-            return None
-
-        version = ua.split("__")[2]
-
-        if version == "5.4.0" or version == "5.4.5" or version == "5.4.5_beta":
-            __date = jsonObj["__date"]
-            video_mediaid = jsonObj["video_mediaid"]
-            video_url = jsonObj["video_url"]
-            video_cdn = jsonObj["video_cdn"]
-            video_network = jsonObj["video_network"]
-            ip = jsonObj["ip"]
-            video_error_code = jsonObj["video_error_code"]
-            video_error_msg = jsonObj["video_error_msg"]
-            video_duration = jsonObj["video_duration"]
-            video_play_duration = jsonObj["video_play_duration"]
-        else:
-            return None
-        """
+            __date = jsonObj["__date"] if "__date" in jsonObj else ""
+            video_mediaid = jsonObj[
+                "video_mediaid"] if "video_mediaid" in jsonObj else ""
+            video_url = jsonObj["video_url"] if "video_url" in jsonObj else ""
+            video_cdn = jsonObj["video_cdh"] if "video_cdh" in jsonObj else ""
+            video_network = jsonObj[
+                "video_network"] if "video_network" in jsonObj else ""
+            ip = jsonObj["ip"] if "ip" in jsonObj else ""
+            video_play_type = jsonObj[
+                "video_play_type"] if "video_play_type" in jsonObj else ""
+            video_play_type_duration = jsonObj[
+                "video_play_type_duration"] if "video_play_type_duration" in jsonObj else ""
+            video_error_code = jsonObj[
+                "video_error_code"] if "video_error_code" in jsonObj else ""
+            video_error_msg = jsonObj[
+                "video_error_msg"] if "video_error_msg" in jsonObj else ""
+            buffer_duration_list = jsonObj[
+                "buffer_duration_list"] if "buffer_duration_list" in jsonObj else ""
+            video_duration = jsonObj[
+                "video_duration"] if "video_duration" in jsonObj else ""
+            video_play_duration = jsonObj[
+                "video_play_duration"] if "video_play_duration" else ""
 
         return (__date, video_mediaid, video_url, version, video_cdn, video_network, ip, video_play_type, video_play_type_duration, video_error_code, video_error_msg, buffer_duration_list, video_duration, video_play_duration)
     except Exception, e:
