bc9690a8cc51911e2ccf735ec764a68aa8e349f1
yurun <miao.work2010@gmail.com>
2015-09-23 15:28:50 +0800

    dev

--- dev/app_picserversweibof6vwt_wapvideodownload.py
+++ dev/app_picserversweibof6vwt_wapvideodownload.py
@@ -229,7 +229,7 @@ result.registerTempTable("temp_table2")
 result = hc.sql("""
 select date,province,isp,cdn,idc,ua,version,video_network, video_error_code,video_error_msg,video_play_type,
         init_timetag,cal_buffer_num.buffer_count,cal_buffer_num.buffer_smaller_500ms_count,cal_buffer_num.buffer_bigger_2min_count,
-        play_process_group
+        play_process_group,sum(video_play_type_duration) as sum_video_play_type_duration
         from temp_table2
         group by date,province,isp,cdn,idc,ua,version,video_network, video_error_code,video_error_msg,video_play_type,
         init_timetag,cal_buffer_num.buffer_count,cal_buffer_num.buffer_smaller_500ms_count,cal_buffer_num.buffer_bigger_2min_count,
