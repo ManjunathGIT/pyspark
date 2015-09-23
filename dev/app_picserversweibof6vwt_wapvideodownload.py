09f8f192dcf89fe1f7df4a7b4934307fcebc8225
yurun <miao.work2010@gmail.com>
2015-09-23 11:57:54 +0800

    dev

--- dev/app_picserversweibof6vwt_wapvideodownload.py
+++ dev/app_picserversweibof6vwt_wapvideodownload.py
@@ -122,6 +122,9 @@ def lineParse(line):
         else:
             return None
 
+        if buffer_duration_list:
+            return None
+
         return (__date, video_mediaid, video_url, ua, video_cdn, video_network, ip, video_play_type, video_play_type_duration, video_error_code, video_error_msg, buffer_duration_list, video_duration, video_play_duration)
     except Exception, e:
         raise
