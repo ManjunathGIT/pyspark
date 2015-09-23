ffc43b75bf53fb31498962542044c915d3effdd3
yurun <miao.work2010@gmail.com>
2015-09-23 14:29:16 +0800

    dev

--- dev/app_picserversweibof6vwt_wapvideodownload.py
+++ dev/app_picserversweibof6vwt_wapvideodownload.py
@@ -134,14 +134,12 @@ def lineParse(line):
         else:
             return None
 
-        columns = (__date, video_mediaid, video_url, ua, video_cdn, video_network, ip, video_play_type,
-                   video_play_type_duration, video_error_code, video_error_msg, buffer_duration_list, video_duration, video_play_duration)
+        if not error_msg:
+            reutrn None
 
-        for column in columns:
-            if not column:
-                return None
+        return (__date, video_mediaid, video_url, ua, video_cdn, video_network, ip, video_play_type,
+                video_play_type_duration, video_error_code, video_error_msg, buffer_duration_list, video_duration, video_play_duration)
 
-        return columns
     except Exception, e:
         raise
 
