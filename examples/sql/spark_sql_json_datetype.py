237a3bd635dba6fc2cd255124d3f43baf3334fc7
yurun <miao.work2010@gmail.com>
2015-09-25 10:29:37 +0800

    dev

--- examples/sql/spark_sql_json_datetype.py
+++ examples/sql/spark_sql_json_datetype.py
@@ -19,6 +19,7 @@ jsonRDD.registerTempTable("temp_table")
 values = hc.sql("select key1, key2 from temp_table").collect()
 """
 
+"""
 source = sc.parallelize(['{"key" : {"key1" : 1, "key2" : "2"}}'])
 
 jsonRDD = hc.jsonRDD(source)
@@ -26,6 +27,15 @@ jsonRDD = hc.jsonRDD(source)
 jsonRDD.registerTempTable("temp_table")
 
 values = hc.sql("select key.key1, key.key2 from temp_table").collect()
+"""
+
+source = sc.parallelize(['{"key" : [1, 2, 3.0]}'])
+
+jsonRDD = hc.jsonRDD(source)
+
+jsonRDD.registerTempTable("temp_table")
+
+values = hc.sql("select key[0], key[1], key[2] from temp_table").collect()
 
 sc.stop()
 
