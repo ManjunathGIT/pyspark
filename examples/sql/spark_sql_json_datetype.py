9820807107f2d3f507a1862a4f772475c50afb43
yurun <miao.work2010@gmail.com>
2015-09-25 10:42:34 +0800

    dev

--- examples/sql/spark_sql_json_datetype.py
+++ examples/sql/spark_sql_json_datetype.py
@@ -51,6 +51,7 @@ values = hc.sql("select key[0], key[1], key[2] from temp_table").collect()
 # values的输出结果：Row(_c0=u'1', _c1=u'2', _c2=u'3.0')，数据类型被全部推断为“int”，也就是说数组的数据类型一定要一致，否则可以引发异常
 """
 
+"""
 source = sc.parallelize(['{"key" : [1, 2 , 3.0]}'])
 
 jsonRDD = hc.jsonRDD(source)
@@ -58,6 +59,19 @@ jsonRDD = hc.jsonRDD(source)
 jsonRDD.registerTempTable("temp_table")
 
 values = hc.sql("select key[0], key[1], key[2] from temp_table").collect()
+
+# values的输出结果：Row(_c0=1.0, _c1=2.0, _c2=3.0)，数据类型被全部推断为“float”
+"""
+
+source = sc.parallelize(
+    ['{"key" : [{"key1" : "value1", "key2" : [1, 2, 3], "key3" : [{"key4" : "value4", "key5" : [4, 5.0, 6]}]}]}'])
+
+jsonRDD = hc.jsonRDD(source)
+
+jsonRDD.registerTempTable("temp_table")
+
+values = hc.sql(
+    "select key[0].key1, key[0].key2[0], key[0].key3[0].key4, key[0].key3[0].key5[1] from temp_table").collect()
 
 sc.stop()
 
