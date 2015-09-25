a950894160c6403e9bd3477c83a436eac5717b1e
yurun <miao.work2010@gmail.com>
2015-09-25 10:35:41 +0800

    dev

--- examples/sql/spark_sql_json_datetype.py
+++ examples/sql/spark_sql_json_datetype.py
@@ -39,6 +39,7 @@ jsonRDD.registerTempTable("temp_table")
 values = hc.sql("select key[0], key[1], key[2] from temp_table").collect()
 """
 
+"""
 source = sc.parallelize(['{"key" : [1, "2" , 3.0]}'])
 
 jsonRDD = hc.jsonRDD(source)
@@ -46,6 +47,17 @@ jsonRDD = hc.jsonRDD(source)
 jsonRDD.registerTempTable("temp_table")
 
 values = hc.sql("select key[0], key[1], key[2] from temp_table").collect()
+
+# values的输出结果：Row(_c0=u'1', _c1=u'2', _c2=u'3.0')，数据类型被全部推断为“int”
+"""
+
+source = sc.parallelize(['{"key" : [1, "2" , 3.0]}'])
+
+jsonRDD = hc.jsonRDD(source)
+
+jsonRDD.registerTempTable("temp_table")
+
+values = hc.sql("select key from temp_table").collect()
 
 sc.stop()
 
