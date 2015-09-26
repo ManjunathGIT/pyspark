0e6b9989f079431bad287f1ca8dffb976da6146e
yurun <miao.work2010@gmail.com>
2015-09-26 09:34:19 +0800

    dev

--- dev/app_picserversweibof6vwt_wapvideodownload.py
+++ dev/app_picserversweibof6vwt_wapvideodownload.py
@@ -23,7 +23,7 @@ hc = HiveContext(sc)
 
 try:
     source = sc.textFile(
-        "/user/hdfs/rawlog/app_picserversweibof6vwt_wapvideodownload/" + timetool.getHDFSPreDayDir(sys.argv[1])).coalesce(30)
+        "/user/hdfs/rawlog/app_picserversweibof6vwt_wapvideodownload/" + timetool.getHDFSPreHourDir(sys.argv[1])).coalesce(30)
 
     pattern = re.compile("^([^`]*)`([^`]*)")
 
