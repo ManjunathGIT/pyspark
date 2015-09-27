066e08872af56a152993646b027c12ac2c0ad7dd
yurun <miao.work2010@gmail.com>
2015-09-27 21:57:04 +0800

    dev

---
+++ examples/app/spark_app_groupByKey.py
@@ -0,0 +1,12 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setAppName("spark_app_groupByKey")
+
+sc = SparkContext(conf=conf)
+
+datas = sc.parallelize(
+    [("a", 1), ("b", 1), ("b", 2), ("c", 1), ("c", 2), ("c", 3)]).groupByKey()
+
+sc.stop()
+
+print[(x, list(y)) for x, y in datas]
