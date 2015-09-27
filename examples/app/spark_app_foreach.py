84110b16b9b5e6eaffa9f4dcc6d2439f227fdd1c
yurun <miao.work2010@gmail.com>
2015-09-27 17:17:44 +0800

    dev

--- examples/app/spark_app_foreach.py
+++ examples/app/spark_app_foreach.py
@@ -1,13 +1,14 @@
 from pyspark import SparkConf, SparkContext
+from logging import Logger
 
 conf = SparkConf().setAppName("spark_app_foreach")
 
 sc = SparkContext(conf=conf)
 
 
-def output(val):
-    print val,
+def log(val):
+    Logger.info("val: " + val)
 
-sc.parallelize(["a", "b", "c"]).foreach(output)
+sc.parallelize(["a", "b", "c"]).foreach(log)
 
 sc.stop()
