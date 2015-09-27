a0c71ac4e4e55e1653259899e525ee631feedad7
yurun <miao.work2010@gmail.com>
2015-09-27 17:11:57 +0800

    dev

--- examples/app/spark_app_foreach.py
+++ examples/app/spark_app_foreach.py
@@ -4,6 +4,6 @@ conf = SparkConf().setAppName("spark_app_foreach")
 
 sc = SparkContext(conf=conf)
 
-sc.parallelize(["a", "b", "c"]).foreach(lambda val: print val,)
+sc.parallelize(["a", "b", "c"]).foreach(lambda val: print val)
 
 sc.stop()
