cfdca83567e0bb1a9f808180a351a45afb5481ea
yurun <miao.work2010@gmail.com>
2015-09-27 17:10:14 +0800

    dev

--- examples/app/spark_app_foreach.py
+++ examples/app/spark_app_foreach.py
@@ -4,12 +4,6 @@ conf = SparkConf().setAppName("spark_app_foreach")
 
 sc = SparkContext(conf=conf)
 
-rdd = sc.parallelize(["a", "b", "c"])
-
-rdd.foreach(lambda val: val.upper())
-
-datas = rdd.collect()
+sc.parallelize(["a", "b", "c"]).foreach(lambda val: print val,)
 
 sc.stop()
-
-print datas
