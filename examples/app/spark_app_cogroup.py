337296aafec2dcde23e9d7aa6c3733ce7e854f18
yurun <miao.work2010@gmail.com>
2015-09-27 10:16:48 +0800

    dev

---
+++ examples/app/spark_app_cogroup.py
@@ -0,0 +1,15 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setAppName("spark_app_cogroup")
+
+sc = SparkContext(conf=conf)
+
+rdd1 = sc.parallelize([("a", 1), ("b", 1)])
+
+rdd2 = sc.parallelize([("a", 2), ("b", 2), ("c", 2)])
+
+datas = rdd1.cogroup(rdd2).collect()
+
+sc.stop()
+
+print datas
