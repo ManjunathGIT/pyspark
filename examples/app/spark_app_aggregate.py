6d162b55a34d40316955ecb3b9ebd92a0378eec6
yurun <miao.work2010@gmail.com>
2015-09-27 09:32:05 +0800

    dev

--- examples/app/spark_app_aggregate.py
+++ examples/app/spark_app_aggregate.py
@@ -4,15 +4,21 @@ conf = SparkConf().setAppName("spark_app_aggregate")
 
 sc = SparkContext(conf=conf)
 
-seqOp = (lambda x, y: (x[0] + y, x[1] + 1))
+seqOp = lambda x, y: x + y
+combOp = lambda x, y: x + y
 
-combOp = (lambda x, y: (x[0] + y[0], x[1] + y[1]))
+data = sc.parallelize([1, 2, 3, 4]).aggregate(0, seqOp, combOp)
 
-#data = sc.parallelize([1, 2, 3, 4]).aggregate((0, 0), seqOp, combOp)
+seqOp = lambda x, y: (x[0] + y, x[1] + 1)
 
-datas = sc.parallelize([1, 2, 3, 4]).collect()
+combOp = lambda x, y: (x[0] + y[0], x[1] + y[1])
+
+data2 = sc.parallelize([1, 2, 3, 4]).aggregate((0, 0), seqOp, combOp)
+
+data3 = sc.parallelize([]).aggregate((0, 0), seqOp, combOp)
 
 sc.stop()
 
-for data in datas:
-    print data[0], data[1]
+print "data:", data
+print "data2:", data2
+print "data3:", data3
