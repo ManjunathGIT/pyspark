e16bc0b453355657c3ad480c6baa085c640fa118
yurun <miao.work2010@gmail.com>
2015-09-27 21:59:07 +0800

    dev

--- examples/app/spark_app_groupByKey.py
+++ examples/app/spark_app_groupByKey.py
@@ -5,7 +5,7 @@ conf = SparkConf().setAppName("spark_app_groupByKey")
 sc = SparkContext(conf=conf)
 
 datas = sc.parallelize(
-    [("a", 1), ("b", 1), ("b", 2), ("c", 1), ("c", 2), ("c", 3)]).groupByKey()
+    [("a", 1), ("b", 1), ("b", 2), ("c", 1), ("c", 2), ("c", 3)]).groupByKey().collect()
 
 sc.stop()
 
