eca2d1f6f1627b584c5284d18aeffa7bca9edc99
yurun <miao.work2010@gmail.com>
2015-09-27 16:53:42 +0800

    dev

--- examples/app/spark_app_foldByKey.py
+++ examples/app/spark_app_foldByKey.py
@@ -6,7 +6,7 @@ conf = SparkConf().setAppName("spark_app_foldByKey")
 sc = SparkContext(conf=conf)
 
 datas = sc.parallelize(
-    [("a", 1), ("b", 1), ("b", 2), ("c", 1), ("c", 2), ("c", 3)]).foldByKey(0, add)
+    [("a", 1), ("b", 1), ("b", 2), ("c", 1), ("c", 2), ("c", 3)]).foldByKey(0, add).collect()
 
 sc.stop()
 
