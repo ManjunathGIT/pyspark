a380327436781a3b8f909bbfada7690c080e2aab
yurun <miao.work2010@gmail.com>
2015-09-28 09:39:03 +0800

    dev

---
+++ examples/app/spark_app_keys.py
@@ -0,0 +1,12 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setAppName("spark_app_keys")
+
+sc = SparkContext(conf=conf)
+
+datas = sc.parallelize(
+    [("a", 1), ("a", 2), ("b", 1), ("c", 1)]).keys().collect()
+
+sc.stop()
+
+print datas
