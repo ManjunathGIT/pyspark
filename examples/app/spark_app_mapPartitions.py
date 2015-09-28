3f016680877c648ff9b571131726222ae7949b25
yurun <miao.work2010@gmail.com>
2015-09-28 10:14:45 +0800

    dev

--- examples/app/spark_app_mapPartitions.py
+++ examples/app/spark_app_mapPartitions.py
@@ -4,7 +4,7 @@ conf = SparkConf().setAppName("spark_app_mapPartitions")
 
 sc = SparkContext(conf=conf)
 
-datas = sc.parallelize([1, 2, 3, 4, 5], 3).mapPartitions(
+datas = sc.parallelize([1, 2, 3, 4, 5, 6], 3).mapPartitions(
     lambda vals: sum(vals)).collect()
 
 sc.stop()
