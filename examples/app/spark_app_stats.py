f65f3856b72579dd28d664de7556c43e68ef4ff5
yurun <miao.work2010@gmail.com>
2015-09-28 15:15:52 +0800

    dev

---
+++ examples/app/spark_app_stats.py
@@ -0,0 +1,12 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setAppName("spark_app_stats")
+
+sc = SparkContext(conf=conf)
+
+data = sc.parallelize([1, 2, 3]).stats()
+
+sc.stop()
+
+# 6
+print data
