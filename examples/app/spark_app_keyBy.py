aef8cc02947fef8e56b9bc848a27954ad699c83a
yurun <miao.work2010@gmail.com>
2015-09-28 09:49:21 +0800

    dev

--- examples/app/spark_app_keyBy.py
+++ examples/app/spark_app_keyBy.py
@@ -8,4 +8,5 @@ datas = sc.parallelize([1, 2, 3, 4, 5]).keyBy(lambda val: val ** 3).collect()
 
 sc.stop()
 
+# [(1, 1), (8, 2), (27, 3), (64, 4), (125, 5)]
 print datas
---
+++ examples/app/spark_app_leftOuterJoin.py
@@ -0,0 +1,15 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setAppName("spark_app_leftOuterJoin")
+
+sc = SparkContext(conf=conf)
+
+rdd1 = sc.parallelize([("a", 1), ("a", 2), ("b", 1)])
+
+rdd2 = sc.parallelize([("a", 3), ("c", 1)])
+
+datas = rdd1.leftOuterJoin(rdd2).collect()
+
+sc.stop()
+
+print datas
