b728aecf5e7e34a7b317e665d3bf3cc8fddcb53f
yurun <miao.work2010@gmail.com>
2015-10-13 17:42:37 +0800

    dev

--- dev/xinqi.py
+++ dev/xinqi.py
@@ -12,7 +12,7 @@ select job_date,cdn,province,isp,ua,play_process_group,version,
 	init_timetag,buffer_count,sum_play_process,sum_video_init_duration,
 	sum_buffer_t_sum,num,idc 
 from datacubic.app_picserversweibof6vwt_wapvideodownload
-where version>='5.4.5' and log_dir='20151013160000'
+where version>='5.4.5' and log_dir>='20151012000000' and log_dir<='20151012230000'
 """
 
 
