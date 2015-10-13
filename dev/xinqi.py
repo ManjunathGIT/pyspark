bbe128c4c5a1812272cb9b5650165c0e8261365a
yurun <miao.work2010@gmail.com>
2015-10-13 19:38:23 +0800

    dev

--- dev/xinqi.py
+++ dev/xinqi.py
@@ -7,35 +7,9 @@ sc = SparkContext(conf=conf)
 
 hc = HiveContext(sc)
 
-spark_sql = '''select job_date,cdn,province,isp,ua,idc,play_process_group,version,init_timetag,buffer_count,
-             sum(sum_play_process) as sum_plapushy_process,
-             sum(sum_video_init_duration) as sum_video_init_duration,
-             sum(sum_buffer_t_sum) as sum_buffer_t_sum,
-             sum(num) as num
-             from(
-             select job_date,cdn,province,isp,ua,play_process_group,version,init_timetag,buffer_count,sum_play_process,sum_video_init_duration,sum_buffer_t_sum,num,idc
-             from datacubic.app_picserversweibof6vwt_wapvideodownload
-             where log_dir>='20151012000000' and log_dir<='20151012230000' and version>='5.4.5'
-             )a
-             group by job_date,cdn,province,isp,ua,idc,play_process_group,version,init_timetag,buffer_count'''
+spark_sql = '''select count(*) from datacubic.www_sinaedgeahsolci14ydn_nginx'''
 
-spark_sql2 = """
-select job_date,cdn,province,isp,ua,play_process_group,version,
-	init_timetag,buffer_count,sum_play_process,sum_video_init_duration,
-	sum_buffer_t_sum,num,idc 
-from datacubic.app_picserversweibof6vwt_wapvideodownload
-where version>='5.4.5' and log_dir>='20151012000000' and log_dir<='20151012230000' group by job_date,cdn,province,isp,ua,idc,play_process_group,version,init_timetag,buffer_count
-"""
-
-
-def noneFilter(row):
-    for column in row:
-        if column == None:
-            return True
-
-    return False
-
-rows = hc.sql(spark_sql).filter(noneFilter).count()
+rows = hc.sql(spark_sql).collect()
 
 sc.stop()
 
