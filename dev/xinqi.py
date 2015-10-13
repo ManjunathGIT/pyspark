0c39fda75611b503f36f4e53ddfe5478d74064bf
yurun <miao.work2010@gmail.com>
2015-10-13 17:31:14 +0800

    dev

--- dev/xinqi.py
+++ dev/xinqi.py
@@ -10,7 +10,7 @@ hc = HiveContext(sc)
 spark_sql = """
 select job_date,cdn,province,isp,ua,play_process_group,version,
 	init_timetag,buffer_count,sum_play_process,sum_video_init_duration,
-	sum_buffer_t_sum,num,temp_split_idc(idc) as idc 
+	sum_buffer_t_sum,num,idc 
 from datacubic.app_picserversweibof6vwt_wapvideodownload
 where log_dir>= '%s' and log_dir<='%s' and version>='5.4.5' and log_dir='20151013160000'
 """
