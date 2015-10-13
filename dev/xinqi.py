45ec512117d4822f370a743d7578d8a2159e526b
yurun <miao.work2010@gmail.com>
2015-10-13 19:40:09 +0800

    dev

--- dev/xinqi.py
+++ dev/xinqi.py
@@ -7,7 +7,7 @@ sc = SparkContext(conf=conf)
 
 hc = HiveContext(sc)
 
-spark_sql = '''select count(*) from datacubic.www_sinaedgeahsolci14ydn_nginx'''
+spark_sql = """select count(*) from datacubic.www_sinaedgeahsolci14ydn_nginx where log_dir='20150915160000'"""
 
 rows = hc.sql(spark_sql).collect()
 
