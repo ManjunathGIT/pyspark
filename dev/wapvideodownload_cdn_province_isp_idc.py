8cd3e814d47e5a0c47bc338701e334e37fc82e89
yurun <miao.work2010@gmail.com>
2015-10-14 13:13:38 +0800

    dev

--- dev/wapvideodownload_cdn_province_isp_idc.py
+++ dev/wapvideodownload_cdn_province_isp_idc.py
@@ -11,6 +11,20 @@ sc = SparkContext(conf=conf)
 
 hc = HiveContext(sc)
 
+source = sc.parallelize(
+    ['{"col1": "row1_col1","col2":"row1_col2","col3":"row1_col3"}', '{"col1": "row2_col1","col2":"row2_col2","col3":"row2_col3"}', '{"col1": "row3_col1","col2":"row3_col2","col3":"row3_col3"}'])
+
+
+table = hc.jsonRDD(source)
+
+table.registerTempTable("temp_mytable")
+
+hc.cacheTable("temp_mytable")
+
+datas = hc.sql("select * from temp_mytable").collect()
+
+datas = hc.sql("select col1 from temp_mytable").collect()
+
 
 def split_idc(idc):
     if idc == None or idc == '' or (not isinstance(idc, basestring)):
