94d3f21048a1cb72249594854596c667f20893f3
yurun <miao.work2010@gmail.com>
2015-10-14 14:14:03 +0800

    dev

--- dev/wapvideodownload_cdn_province_isp_idc.py
+++ dev/wapvideodownload_cdn_province_isp_idc.py
@@ -94,7 +94,7 @@ table = hc.sql("""select '1' as job_date,cdn,province,isp,ua,idc,play_process_gr
              sum(num) as num
              from(
              select cdn,province,isp,ua,play_process_group,version,init_timetag,buffer_count,sum_play_process,sum_video_init_duration,sum_buffer_t_sum,num,
-             idc
+             temp_split_idc(idc) as idc
              from datacubic.app_picserversweibof6vwt_wapvideodownload
              where log_dir= '20151012110000' and version>='5.4.5'
              )a
