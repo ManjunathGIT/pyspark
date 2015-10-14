efe2c3a56bda7458635895ee7525c57c89a24545
yurun <miao.work2010@gmail.com>
2015-10-14 16:32:49 +0800

    dev

--- examples/sql/spark_sql_cache.py
+++ examples/sql/spark_sql_cache.py
@@ -24,7 +24,7 @@ def myupper(value):
 hc.registerFunction("temp_myupper", myupper)
 
 mytable = hc.sql(
-    "select temp_myupper(col1) as col1, col2, col3 from temp_source")
+    "select upper(col1) as col1, col2, col3 from temp_source")
 
 mytable.registerTempTable("temp_mytable")
 
