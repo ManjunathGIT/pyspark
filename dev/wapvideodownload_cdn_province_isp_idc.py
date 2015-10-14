2d51b294d997e50f634ff609bf825d184a02e335
yurun <miao.work2010@gmail.com>
2015-10-14 14:48:11 +0800

    dev

--- dev/wapvideodownload_cdn_province_isp_idc.py
+++ dev/wapvideodownload_cdn_province_isp_idc.py
@@ -11,21 +11,6 @@ sc = SparkContext(conf=conf)
 
 hc = HiveContext(sc)
 
-"""
-source = sc.parallelize(
-    ['{"col1": "row1_col1","col2":"row1_col2","col3":"row1_col3"}', '{"col1": "row2_col1","col2":"row2_col2","col3":"row2_col3"}', '{"col1": "row3_col1","col2":"row3_col2","col3":"row3_col3"}'])
-
-
-table = hc.jsonRDD(source)
-
-table.registerTempTable("temp_mytable")
-
-hc.cacheTable("temp_mytable")
-
-datas = hc.sql("select * from temp_mytable").collect()
-
-datas = hc.sql("select col1 from temp_mytable").collect()
-
 
 def split_idc(idc):
     if idc == None or idc == '' or (not isinstance(idc, basestring)):
@@ -52,7 +37,10 @@ spark_sql = '''select '1' as job_date,cdn,province,isp,ua,idc,play_process_group
              where log_dir= '20151012110000' and version>='5.4.5' limit 10
              )a
              group by cdn,province,isp,ua,idc,play_process_group,version,init_timetag,buffer_count'''
-rows_rdd = hc.sql(spark_sql)
+result = hc.sql(spark_sql).collect()
+result_rdd = sc.parallelize(result)
+
+rows_rdd = hc.inferSchema(result_rdd)
 rows_rdd.registerTempTable("temp_rdd")
 hc.cacheTable("temp_rdd")
 #--------------------------2.1 播放总请求量-----------------------
@@ -72,44 +60,5 @@ spark_sql = '''select job_date,cdn,province,isp,idc,sum(num) as num
                group by job_date,cdn,province,isp,idc'''
 rows = hc.sql(spark_sql).collect()
 print 222222222222222222222
-"""
-
-
-def split_idc(idc):
-    if idc == None or idc == '' or (not isinstance(idc, basestring)):
-        return ''
-    else:
-        words = idc.split('.')
-        if len(words) >= 2:
-            return words[0] + '.' + words[1]
-        else:
-            return ''
-
-hc.registerFunction("temp_split_idc", split_idc)
-
-source = hc.sql("""select cdn,province,isp,ua,play_process_group,version,init_timetag,buffer_count,sum_play_process,sum_video_init_duration,sum_buffer_t_sum,num,
-             temp_split_idc(idc) as idc
-             from datacubic.app_picserversweibof6vwt_wapvideodownload
-             where log_dir= '20151012110000' and version>='5.4.5'""")
-
-source.registerTempTable("temp_source_table")
-
-table = hc.sql("""select '1' as job_date,cdn,province,isp,ua,idc,play_process_group,version,init_timetag,buffer_count,
-             sum(sum_play_process) as sum_play_process,
-             sum(sum_video_init_duration) as sum_video_init_duration,
-             sum(sum_buffer_t_sum) as sum_buffer_t_sum,
-             sum(num) as num
-             from temp_source_table
-             group by cdn,province,isp,ua,idc,play_process_group,version,init_timetag,buffer_count""")
-
-table.cache()
-
-table.registerTempTable("temp_mytable")
-
-# hc.cacheTable("temp_mytable")
-
-datas = hc.sql("select * from temp_mytable limit 10").collect()
-
-datas = hc.sql("select count(*) from temp_mytable").collect()
 
 sc.stop()
