dd3d6331251d67dbfb37dee51627483b298f139b
yurun <miao.work2010@gmail.com>
2015-10-15 17:10:32 +0800

    dev

---
+++ 1.5.1/examples/sql/spark_sql_dataframe_where_or_filter.py
@@ -0,0 +1,19 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext, Row
+
+conf = SparkConf().setAppName("spark_sql_dataframe_show")
+
+sc = SparkContext(conf=conf)
+
+sqlCtx = SQLContext(sc)
+
+lines = sc.parallelize(["a,1", "b,2", "c,3"])
+
+people = lines.map(lambda line: line.split(",")).map(
+    lambda words: Row(name=words[0], age=int(words[1])))
+
+schemaPeople = sqlCtx.createDataFrame(people)
+
+schemaPeople.where("name = b").where(schemaPeople["age"] > 1).show()
+
+sc.stop()
