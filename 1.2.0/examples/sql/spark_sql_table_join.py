d493c6fd3d2282d8232682c96dde0a0ec3cd3111
yurun <miao.work2010@gmail.com>
2015-10-19 15:24:18 +0800

    dev

---
+++ 1.2.0/examples/sql/spark_sql_table_join.py
@@ -0,0 +1,66 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import HiveContext, Row
+
+conf = SparkConf().setAppName("spark_sql_table_join")
+
+sc = SparkContext(conf=conf)
+
+sqlCtx = HiveContext(sc)
+
+line1 = sc.parallelize(["name1 a", "name3 c", "name4 d"])
+
+line2 = sc.parallelize(["name1 1", "name2 2", "name3 3"])
+
+word1 = line1.map(lambda line: line.split(" "))
+
+word2 = line2.map(lambda line: line.split(" "))
+
+table1 = word1.map(lambda words: Row(name=words[0], title=words[1]))
+
+table2 = word2.map(lambda words: Row(name=words[0], fraction=words[1]))
+
+tableSchema1 = sqlCtx.inferSchema(table1)
+
+tableSchema2 = sqlCtx.inferSchema(table2)
+
+tableSchema1.registerTempTable("table1")
+
+tableSchema2.registerTempTable("table2")
+
+
+def printRows(rows):
+    if rows:
+        for row in rows:
+            print row
+
+# inner join
+rows = sqlCtx.sql(
+    "select table1.name, table1.title, table2.fraction from table1 inner join table2 on table1.name = table2.name").collect()
+
+printRows(rows)
+
+print "============================================="
+
+# left outer join
+rows = sqlCtx.sql(
+    "select table1.name, table1.title, table2.fraction from table1 left outer join table2 on table1.name = table2.name").collect()
+
+printRows(rows)
+
+# right outer join
+rows = sqlCtx.sql(
+    "select table1.name, table1.title, table2.fraction from table1 right outer join table2 on table1.name = table2.name").collect()
+
+print "============================================="
+
+printRows(rows)
+
+# full outer join
+rows = sqlCtx.sql(
+    "select table1.name, table1.title, table2.fraction from table1 full outer join table2 on table1.name = table2.name").collect()
+
+print "============================================="
+
+printRows(rows)
+
+sc.stop()
--- tmp/test.py
+++ tmp/test.py
@@ -1,7 +1,2 @@
-mytuple = (1,2,3)
-
-mylist = list(mytuple)
-
-mylist[0] = 4
-
-print mylist
+print globals()
+print locals()
