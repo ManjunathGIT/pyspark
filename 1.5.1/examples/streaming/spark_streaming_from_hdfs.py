cd27aac4a3828c98f7fd78ec5173115c26aca6a3
yurun <miao.work2010@gmail.com>
2015-10-20 15:10:46 +0800

    dev

--- 1.5.1/examples/streaming/spark_streaming_from_hdfs.py
+++ 1.5.1/examples/streaming/spark_streaming_from_hdfs.py
@@ -10,7 +10,7 @@ streamingCtx = StreamingContext(sc, 10)
 filePathDStream = streamingCtx.textFileStream(
     "hdfs://dip.cdh5.dev:8020/user/yurun/data/")
 
-
+"""
 def convertRDD(filePathRDD):
     return filePathRDD.map(lambda filePath: filePathRDD.context.textFile(filePath))
 
@@ -21,6 +21,12 @@ wordcounts = fileLineDStream.flatMap(
     lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda countA, countB: countA + countB)
 
 wordcounts.pprint()
+"""
+
+count = filePathDStream.transform(
+    lambda rdd: rdd.map(lambda filePath: rdd.context.textFile(filePath))).count()
+
+print count
 
 streamingCtx.start()
 
