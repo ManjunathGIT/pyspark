b438fedcfffe20f5938c9ab56168f46a6db43802
yurun <miao.work2010@gmail.com>
2015-10-22 10:34:50 +0800

    dev

--- 1.2.0/dev/sae_nginx_top_url.py
+++ 1.2.0/dev/sae_nginx_top_url.py
@@ -6,6 +6,8 @@ conf = SparkConf().setAppName("sae_nginx_top_url")
 
 sc = SparkContext(conf=conf)
 
+hc = HiveContext(sc)
+
 
 def mysqldb(host, port, user, passwd, db, sql):
     conn = None
@@ -47,7 +49,30 @@ for domain in top_domain_list:
     top_domain_dict[domain[0]] = i
 
     i = i + 1
-    
+
 print top_domain_dict
+
+jsonRDD = hc.jsonFile(
+    "/user/hdfs/rawlog/app_saesinacomkafka12345_nginx/2015_10_22/09")
+
+hc.registerRDDAsTable(jsonRDD, "temp_schema")
+
+spark_sql = '''select domain,domain_order,url,cast(sum(body_bytes_sent) as bigint) as flow,count(1) as num from (
+                select domain,
+                temp_if_in_top_10_domain(domain) as domain_order,
+                split(request,'\\\\?')[0] as url,
+                body_bytes_sent
+                from temp_schema
+                where body_bytes_sent>0 and temp_if_in_top_10_domain(domain)!='no'
+                )A
+           group by domain,domain_order,url
+           order by flow desc limit 100
+'''
+
+rows_temp = hc.sql(spark_sql).map(lambda row: (
+    (row.domain, row.domain_order, row.url, row.flow, row.num), None)).collect()
+
+for val in rows_temp:
+    print val
 
 sc.stop()
