b654ebbbc4002b340956894e49fa64184b762c3f
yurun <miao.work2010@gmail.com>
2015-10-22 10:09:41 +0800

    dev

---
+++ 1.2.0/dev/sae_nginx_top_url.py
@@ -0,0 +1,44 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import HiveContext
+
+conf = SparkConf().setAppName("sae_nginx_top_url")
+
+sc = SparkContext(conf=conf)
+
+
+def mysqldb(host, port, user, passwd, db, sql):
+    conn = None
+
+    cur = None
+
+    try:
+        conn = MySQLdb.connect(host=host, port=port, user=user,
+                               passwd=passwd, db=db, charset="utf8")
+
+    cur = conn.cursor()
+
+    result = cur.execute(sql)
+
+    return cur.fetchall()
+    except Exception, e:
+        pass
+    finally:
+        if cur:
+            try:
+                cur.close()
+            except Exception, e:
+                pass
+
+        if conn:
+            try:
+                conn.close()
+            except Exception, e:
+                pass
+
+top_domain_list = mysqldb("m3353i.apollo.grid.sina.com.cn", 3353, "data_history", "f3u4w8n7b3h", "sae",
+                          "select domain from (select domain,round(sum(flow)/1024/1024,0)  as flow_MB     from sae.sae_nginx_flow flow    where DATE_SUB(CURDATE(), INTERVAL 1 DAY) = date group by domain order by flow_MB desc limit 20)A")
+
+for val in top_domain_list:
+    print val
+
+sc.stop()
