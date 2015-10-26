b4eecf1d1f730e6acbb9049426a4705216a9f5cb
yurun <miao.work2010@gmail.com>
2015-10-26 19:51:09 +0800

    dev

--- 1.2.0/examples/sql/spark_sql_datatype2.py
+++ 1.2.0/examples/sql/spark_sql_datatype2.py
@@ -31,7 +31,7 @@ sc = SparkContext(conf=conf)
 
 hc = SQLContext(sc)
 
-source = sc.parallelize([(-129, )])
+source = sc.parallelize([(-128, )])
 
 schema = StructType([StructField("byte", ByteType(), False)])
 
