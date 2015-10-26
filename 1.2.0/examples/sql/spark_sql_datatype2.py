a77811dbed3ceb3427f0950bd51185adfdb0f369
yurun <miao.work2010@gmail.com>
2015-10-26 19:54:22 +0800

    dev

--- 1.2.0/examples/sql/spark_sql_datatype2.py
+++ 1.2.0/examples/sql/spark_sql_datatype2.py
@@ -31,7 +31,7 @@ sc = SparkContext(conf=conf)
 
 hc = HiveContext(sc)
 
-source = sc.parallelize([(-128, 256)])
+source = sc.parallelize([(-128, 255)])
 
 schema = StructType([StructField("col1", ByteType(), False),
                      StructField("col2", ByteType(), False)])
