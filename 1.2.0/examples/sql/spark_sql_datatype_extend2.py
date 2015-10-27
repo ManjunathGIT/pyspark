6ce5671d0a42e13ff7370be764c609a2dabfb989
yurun <miao.work2010@gmail.com>
2015-10-27 10:15:04 +0800

    dev

--- 1.2.0/examples/sql/spark_sql_datatype_extend2.py
+++ 1.2.0/examples/sql/spark_sql_datatype_extend2.py
@@ -41,13 +41,10 @@ table = hc.applySchema(source, schema)
 
 table.registerAsTable("temp_table")
 
-"""
+# java.lang.ClassCastException: java.math.BigInteger cannot be cast to
+# java.lang.Long
 rows = hc.sql(
     "select cast(col1 as decimal(38, 0)) + cast(col2 as decimal(38, 0)) from temp_table").collect()
-"""
-
-rows = hc.sql(
-    "select cast(col1 as decimal(38, 0)) + col2 from temp_table").collect()
 
 sc.stop()
 
---
+++ 1.2.0/examples/sql/spark_sql_datatype_extend3.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+
+"""Spark SQL DataType
+
+ByteType: int
+ShortType: int
+IntegerType: int
+LongType: long
+FloatType: float
+DoubleType: float
+Decimal: Decimal
+StringType: ""
+BinaryType: ignore
+BooleanType: bool
+TimestampType: datetime
+DateType: date
+ArrayType: list
+MapType: dict
+StructType: tuple
+"""
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import HiveContext
+import decimal
+from datetime import datetime, date
+from pyspark.sql import StructType, StructField, LongType
+
+conf = SparkConf().setAppName("spark_sql_datatype_extend3")
+
+sc = SparkContext(conf=conf)
+
+hc = HiveContext(sc)
+
+source = sc.parallelize(
+    ["85070591730234615847396907784232501249", "85070591730234615847396907784232501249"])
+
+rows = source.map(lambda value: int(value)).collect()
+
+sc.stop()
+
+for row in rows:
+    print row
