9bc9f981a85770b2244571d1e3f9eeb45e0a0543
yurun <miao.work2010@gmail.com>
2015-10-27 17:16:17 +0800

    dev

---
+++ 1.2.0/dev/wapvideodownload_ua_play_duration.py
@@ -0,0 +1,18 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import HiveContext
+from decimal import Decimal
+from datetime import datetime, date
+from pyspark.sql import StructType, StructField, ByteType, ShortType, IntegerType, LongType, FloatType, DoubleType, DecimalType, StringType, BooleanType, TimestampType, DateType, ArrayType, MapType
+
+conf = SparkConf().setAppName("spark_sql_datatype")
+
+sc = SparkContext(conf=conf)
+
+hc = HiveContext(sc)
+
+rows = hc.sql("select count(distinct(video_duration_group)) from app_picserversweibof6vwt_wapvideodownload where log_dir = 20151027150000").collect()
+
+sc.stop()
+
+for row in rows:
+    print row
