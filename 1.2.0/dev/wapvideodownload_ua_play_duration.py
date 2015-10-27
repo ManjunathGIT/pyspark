339be8813fb5717931a036c7715e846ad49891bc
yurun <miao.work2010@gmail.com>
2015-10-27 17:32:19 +0800

    dev

--- 1.2.0/dev/wapvideodownload_ua_play_duration.py
+++ 1.2.0/dev/wapvideodownload_ua_play_duration.py
@@ -10,7 +10,18 @@ sc = SparkContext(conf=conf)
 
 hc = HiveContext(sc)
 
-rows = hc.sql("select count(distinct(video_duration_group)) from datacubic.app_picserversweibof6vwt_wapvideodownload where log_dir = 20151027150000").collect()
+spark_sql = '''select '%s' as job_date,ua,version,video_network,video_is_autoplay,
+             video_play_duration_group,video_duration_group,play_process_group,
+             sum(num) as num
+             from datacubic.app_picserversweibof6vwt_wapvideodownload
+             where log_dir>='%s' and log_dir<='%s' and version>='5.4.5'
+             and play_process_group!='NoPlay' and play_process_group!='-'
+             group by ua,version,video_network,video_is_autoplay,
+             video_play_duration_group,video_duration_group,play_process_group''' % ("20151027150000", "20151027150000", "20151027150000")
+
+#rows = hc.sql("select count(distinct(video_duration_group)) from datacubic.app_picserversweibof6vwt_wapvideodownload where log_dir = 20151027150000").collect()
+
+rows = hc.sql(spark_sql)
 
 sc.stop()
 
