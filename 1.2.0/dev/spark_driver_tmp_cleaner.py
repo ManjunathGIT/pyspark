3a7bce0290edc42c0a16b5ab7685a69ab6417736
yurun <miao.work2010@gmail.com>
2015-10-30 12:57:09 +0800

    dev

--- 1.2.0/dev/spark_driver_tmp_cleaner.py
+++ 1.2.0/dev/spark_driver_tmp_cleaner.py
@@ -1,7 +1,7 @@
 import time
 import os
 
-clean_endtime = time.time()
+clean_endtime = time.time() - (24 * 3600)
 
 print clean_endtime
 
@@ -10,7 +10,7 @@ tmp = "/tmp"
 paths = os.listdir(tmp)
 
 for path in paths:
-	spark_clean_dir = os.path.join(tmp, path)
+    spark_clean_dir = os.path.join(tmp, path)
 
-	if os.path.isdir(spark_clean_dir) and (path.startswith("spark-") or path.endswith("_resources")):
-		print os.path.getctime(spark_clean_dir)
+    if os.path.isdir(spark_clean_dir) and (path.startswith("spark-") or path.endswith("_resources")) and os.path.getmtime(spark_clean_dir) <= clean_endtime:
+        print spark_clean_dir
