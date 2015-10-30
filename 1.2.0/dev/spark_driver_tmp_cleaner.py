ee71ece2ee527cce5746adc2245bb300fadf0373
yurun <miao.work2010@gmail.com>
2015-10-30 11:25:56 +0800

    dev

--- 1.2.0/dev/spark_driver_tmp_cleaner.py
+++ 1.2.0/dev/spark_driver_tmp_cleaner.py
@@ -10,4 +10,4 @@ paths = os.listdir("/tmp")
 for path in paths:
     # if os.path.isdir(path) and (path.startswith("spark-") or
     # path.endswith("resources")):
-    print path, os.path.getmtime(path)
+    print path
