7639f967f3e5d46789eb510decf03388f471cf11
yurun <miao.work2010@gmail.com>
2015-10-30 14:48:21 +0800

    dev

---
+++ 1.2.0/examples/sql/spark_sql_datatype_str_bool_binary_none.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+
+"""Spark SQL DataType
+
+ByteType: int
+ShortType: int
+IntegerType: int
+LongType: long
+FloatType: float
+DoubleType: float
+Decimal: Decimal
+StringType: ""
+BinaryType: ignore
+BooleanType: bool
+TimestampType: datetime
+DateType: date
+ArrayType: list
+MapType: dict
+StructType: tuple
+"""
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import HiveContext
+from pyspark.sql import StructType, StructField, StringType, BooleanType, BinaryType, NullType
+
+conf = SparkConf().setAppName("spark_sql_datatype_str_bool_binary_none")
+
+sc = SparkContext(conf=conf)
+
+hc = HiveContext(sc)
+
+source = sc.parallelize([("str", False, bytearray(range(0, 256), None))])
+
+schema = StructType([StructField("str", StringType(), False), StructField("bool", BooleanType(
+), False), StructField("bytes", BinaryType(), False), StructField("none", NullType(), True)])
+
+table = hc.applySchema(source, schema)
+
+table.registerAsTable("temp_table")
+
+rows = hc.sql(
+    "select str, bool, bytes, none from temp_table").collect()
+
+sc.stop()
+
+for row in rows:
+    print row
