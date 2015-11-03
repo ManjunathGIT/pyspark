d44711a2978eb7449ce7dd88379f3beaad39fd7a
yurun <miao.work2010@gmail.com>
2015-11-03 15:45:02 +0800

    dev

--- tmp/test.py
+++ tmp/test.py
@@ -14,7 +14,7 @@ print zip(row.__fields__, tuple(row))
 
 from collections import namedtuple
 
-row = namedtuple("NamedRow", ["key1", "key2", "key3"])
+NamedRow = namedtuple("NamedRow", ["key1", "key2", "key3"])
 
 row = NamedRow(key1="val1", key2="val2", key3="val3")
 
