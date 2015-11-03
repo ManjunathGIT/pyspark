d9018b09bf18626e1d7d6679ac17d9adfb4c53ce
yurun <miao.work2010@gmail.com>
2015-11-03 15:35:02 +0800

    dev

--- tmp/test.py
+++ tmp/test.py
@@ -1,1 +1,9 @@
-print type({})
+datas = {"key1": "val1", "key2": "val2", "key3": "val3"}
+
+print sorted(datas.items())
+
+from pyspark.sql.types import Row
+
+row = Row(key1="val1", key2="val2", key3="val3")
+
+print row.__fields__
