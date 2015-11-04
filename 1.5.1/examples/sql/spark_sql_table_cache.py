5b86c6b5de1a7e06509261385a88ce30202a7f5c
yurun <miao.work2010@gmail.com>
2015-11-04 10:14:40 +0800

    dev

---
+++ 1.5.1/examples/sql/spark_sql_table_cache.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import HiveContext
+from pyspark.sql.types import Row
+
+conf = SparkConf().setAppName("spark_sql_table_cache")
+
+sc = SparkContext(conf=conf)
+
+hc = HiveContext(sc)
+
+source = sc.parallelize(
+    ['{"col1": "row1_col1","col2":"row1_col2","col3":"row1_col3"}', '{"col1": "row2_col1","col2":"row2_col2","col3":"row2_col3"}', '{"col1": "row3_col1","col2":"row3_col2","col3":"row3_col3"}'])
+
+
+sourceRDD = hc.jsonRDD(source)
+
+sourceRDD.registerTempTable("source")
+
+datas = hc.sql("select * from source").collect()
+
+printRows(datas)
+
+# hc.uncacheTable("temp_mytable")
+
+sc.stop()
