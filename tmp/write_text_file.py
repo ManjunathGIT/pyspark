a42ebbeb49306ccd7bbc32663122cfd78b2046b7
yurun <miao.work2010@gmail.com>
2015-11-04 15:02:42 +0800

    dev

---
+++ tmp/write_text_file.py
@@ -0,0 +1,7 @@
+file = open("/tmp/datas", "w")
+
+for index in xrange(0, 10000):
+    file.write("col1_" + str(random.randint(1, 100)) + "\tcol1_" +
+               str(random.randint(1, 100)) + "\tcol1_" + str(random.randint(1, 100)) + "\n")
+
+file.close()
