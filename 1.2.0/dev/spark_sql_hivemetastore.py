4a857904511aca6d2562a1693b4e6fa269e3ab40
yurun <miao.work2010@gmail.com>
2015-11-09 13:55:47 +0800

    dev

--- 1.2.0/dev/spark_sql_hivemetastore.py
+++ 1.2.0/dev/spark_sql_hivemetastore.py
@@ -7,7 +7,7 @@ sc = SparkContext(conf=conf)
 
 hc = HiveContext(sc)
 
-rows = hc.sql("select c from yurun.tablep").collect()
+rows = hc.sql("select func.ipToLocationBySina('10.13.4.44') from yurun.tablep").collect()
 
 sc.stop()
 
