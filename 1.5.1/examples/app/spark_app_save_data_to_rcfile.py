a12cdc1cd6e8b4c8a53d6411d9405c18642f058d
yurun <miao.work2010@gmail.com>
2015-11-09 20:54:48 +0800

    dev

---
+++ 1.5.1/examples/app/spark_app_save_data_to_rcfile.py
@@ -0,0 +1,17 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setAppName("spark_app_save_data_to_rcfile")
+
+sc = SparkContext(conf=conf)
+
+lines = ["1\t2\t3", "4\t5\t6", "7\t8\t9"]
+
+pairRDD = sc.parallelize(lines).map(lambda line: (None, line))
+
+pairRDD.saveAsHadoopFile(path="hdfs://dip.cdh5.dev:8020/user/yurun/rcfile/",
+                         outputFormatClass="org.apache.hadoop.hive.ql.io.RCFileOutputFormat",
+                         keyClass="org.apache.hadoop.io.NullWritable",
+                         valueClass="org.apache.hadoop.hive.serde2.columnar.BytesRefArrayWritable",
+                         valueConverter="com.sina.dip.spark.converter.StringToBytesRefArrayWritableConverter")
+
+sc.stop()
