4908f4e5423b509854e16366d00f5ad7566609b3
yurun <miao.work2010@gmail.com>
2015-11-09 20:59:34 +0800

    dev

--- 1.5.1/examples/app/spark_app_save_data_to_rcfile.py
+++ 1.5.1/examples/app/spark_app_save_data_to_rcfile.py
@@ -8,7 +8,7 @@ lines = ["1\t2\t3", "4\t5\t6", "7\t8\t9"]
 
 pairRDD = sc.parallelize(lines).map(lambda line: (None, line))
 
-pairRDD.saveAsHadoopFile(path="hdfs://dip.cdh5.dev:8020/user/yurun/rcfile/",
+pairRDD.saveAsHadoopFile(path="hdfs://dip.cdh5.dev:8020/user/yurun/rcfile",
                          outputFormatClass="org.apache.hadoop.hive.ql.io.RCFileOutputFormat",
                          keyClass="org.apache.hadoop.io.NullWritable",
                          valueClass="org.apache.hadoop.hive.serde2.columnar.BytesRefArrayWritable",
