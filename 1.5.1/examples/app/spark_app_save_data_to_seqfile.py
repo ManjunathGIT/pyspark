d15be3995c1b27677186146ddaef651fa43feb20
yurun <miao.work2010@gmail.com>
2015-11-10 15:05:52 +0800

    dev

--- 1.5.1/examples/app/spark_app_save_data_to_seqfile.py
+++ 1.5.1/examples/app/spark_app_save_data_to_seqfile.py
@@ -6,7 +6,7 @@ sc = SparkContext(conf=conf)
 
 arrays = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
 
-pairRDD = sc.parallelize(lines).map(lambda array: (array, None))
+pairRDD = sc.parallelize(arrays).map(lambda array: (array, None))
 
 pairRDD.saveAsHadoopFile(path="hdfs://dip.cdh5.dev:8020/user/yurun/seqfile",
                          outputFormatClass="org.apache.hadoop.mapred.SequenceFileOutputFormat",
