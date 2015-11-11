39a364ef63ebeba9e23b35a2b9f443e9d96d0712
yurun <miao.work2010@gmail.com>
2015-11-11 11:30:49 +0800

    dev

--- 1.5.1/examples/app/spark_app_read_data_from_rcfile.py
+++ 1.5.1/examples/app/spark_app_read_data_from_rcfile.py
@@ -8,7 +8,7 @@ lineRDD = sc.hadoopFile(path="hdfs://dip.cdh5.dev:8020/user/yurun/rcfile",
                         inputFormatClass="org.apache.hadoop.hive.ql.io.RCFileInputFormat",
                         keyClass="org.apache.hadoop.io.NullWritable",
                         valueClass="org.apache.hadoop.hive.serde2.columnar.BytesRefArrayWritable",
-                        valueConverter="com.sina.dip.spark.converter.BytesRefArrayWritableToStringConverter").map(lambda pair: pair[1])
+                        valueConverter="com.sina.dip.spark.converter.BytesRefArrayWritableToObjectArrayConverter").map(lambda pair: pair[1])
 
 lines = lineRDD.collect()
 
--- 1.5.1/examples/app/spark_app_save_data_to_rcfile.py
+++ 1.5.1/examples/app/spark_app_save_data_to_rcfile.py
@@ -4,9 +4,9 @@ conf = SparkConf().setAppName("spark_app_save_data_to_rcfile")
 
 sc = SparkContext(conf=conf)
 
-lines = ["1\t2\t3", "4\t5\t6", "7\t8\t9"]
+rows = [("1", "2", "3"), ("4", "5", "6"), ("7", "8", "9")]
 
-pairRDD = sc.parallelize(lines).map(lambda line: (None, line))
+pairRDD = sc.parallelize(rows).map(lambda row: (None, row))
 
 conf = {"hive.io.rcfile.column.number.conf": "3"}
 
@@ -14,6 +14,6 @@ pairRDD.saveAsHadoopFile(path="hdfs://dip.cdh5.dev:8020/user/yurun/rcfile",
                          outputFormatClass="com.sina.dip.spark.output.DipRCFileOutputFormat",
                          keyClass="org.apache.hadoop.io.NullWritable",
                          valueClass="org.apache.hadoop.hive.serde2.columnar.BytesRefArrayWritable",
-                         valueConverter="com.sina.dip.spark.converter.StringToBytesRefArrayWritableConverter", conf=conf)
+                         valueConverter="com.sina.dip.spark.converter.ObjectArrayToBytesRefArrayWritableConverter", conf=conf)
 
 sc.stop()
