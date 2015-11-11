66f9fbef4322159199da3168ef6dc324a4887b20
yurun <miao.work2010@gmail.com>
2015-11-11 14:56:20 +0800

    dev

--- 1.5.1/examples/app/spark_app_read_data_from_rcfile.py
+++ 1.5.1/examples/app/spark_app_read_data_from_rcfile.py
@@ -4,15 +4,15 @@ conf = SparkConf().setAppName("spark_app_read_data_from_rcfile")
 
 sc = SparkContext(conf=conf)
 
-lineRDD = sc.hadoopFile(path="hdfs://dip.cdh5.dev:8020/user/yurun/rcfile",
-                        inputFormatClass="org.apache.hadoop.hive.ql.io.RCFileInputFormat",
-                        keyClass="org.apache.hadoop.io.NullWritable",
-                        valueClass="org.apache.hadoop.hive.serde2.columnar.BytesRefArrayWritable",
-                        valueConverter="com.sina.dip.spark.converter.BytesRefArrayWritableToObjectArrayConverter").map(lambda pair: pair[1])
+rowRDD = sc.hadoopFile(path="hdfs://dip.cdh5.dev:8020/user/yurun/rcfile",
+                       inputFormatClass="org.apache.hadoop.hive.ql.io.RCFileInputFormat",
+                       keyClass="org.apache.hadoop.io.LongWritable",
+                       valueClass="org.apache.hadoop.hive.serde2.columnar.BytesRefArrayWritable",
+                       valueConverter="com.sina.dip.spark.converter.BytesRefArrayWritableToObjectArrayConverter")
 
-lines = lineRDD.collect()
+pairs = rowRDD.collect()
 
-for line in lines:
-    print line
+for pair in pairs:
+    print pair[0], pair[1]
 
 sc.stop()
