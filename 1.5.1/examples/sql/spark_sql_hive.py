1e4751259ed973756a654739e17b2f3ba3cc026d
yurun <miao.work2010@gmail.com>
2015-12-16 18:28:55 +0800

    dev

--- 1.5.1/examples/sql/spark_sql_hive.py
+++ 1.5.1/examples/sql/spark_sql_hive.py
@@ -3,6 +3,8 @@ from pyspark.sql import HiveContext
 
 conf = SparkConf().setAppName("spark_sql_hive")
 
+conf.set("hive.metastore.uris", "thrift://10.13.4.44:9083")
+
 sc = SparkContext(conf=conf)
 
 hc = HiveContext(sc)
