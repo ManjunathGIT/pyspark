8e671186e5f6168ec7c21d22914f0b9dd0d81517
yurun <miao.work2010@gmail.com>
2015-12-31 15:18:08 +0800

    dev

--- dip/test.py
+++ dip/test.py
@@ -5,9 +5,13 @@ user = os.getlogin()
 
 groups = commands.getoutput("groups " + user)
 
+print groups
+
 teams = {"datacubic": "datacubic_conf", "topweibo": "topweibo_conf"}
 
 for team in teams:
+	print team
+	
     if team in groups:
         print teams[team]
 
