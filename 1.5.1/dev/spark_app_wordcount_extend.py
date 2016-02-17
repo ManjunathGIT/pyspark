98b741eb89ee3377ab47ea51f67f3bb9a91dbce5
yurun <miao.work2010@gmail.com>
2016-02-17 11:42:49 +0800

    dev

--- 1.5.1/dev/spark_app_wordcount_extend.py
+++ 1.5.1/dev/spark_app_wordcount_extend.py
@@ -2,11 +2,15 @@ from pyspark import SparkConf, SparkContext
 
 conf = SparkConf()
 
-conf.setAppName("spark_app_wordcount")
+conf.setAppName("spark_app_wordcount_extend")
 
 sc = SparkContext(conf=conf)
 
-lines = sc.textFile("/user/yurun/spark/textfile/")
+lines = sc.newAPIHadoopFile(
+    "/user/yurun/spark/textfile/",
+    "org.apache.hadoop.mapreduce.lib.input.CombineFileInputFormat",
+    "org.apache.hadoop.io.LongWritable",
+    "org.apache.hadoop.io.Text")
 
 words = lines.flatMap(lambda line: line.split("\t"))
 
