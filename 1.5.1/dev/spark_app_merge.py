19cda2e5ce70552c3b4afad605018241fc83933d
yurun <miao.work2010@gmail.com>
2016-02-22 14:04:52 +0800

    dev

--- 1.5.1/dev/spark_app_merge.py
+++ 1.5.1/dev/spark_app_merge.py
@@ -7,7 +7,7 @@ conf.setAppName("spark_app_merge")
 sc = SparkContext(conf=conf)
 
 hadoopConf = {"mapreduce.input.fileinputformat.inputdir": "/user/yurun/spark/textfile/",
-              "mapreduce.input.fileinputformat.input.dir.recursive": true}
+              "mapreduce.input.fileinputformat.input.dir.recursive": "true"}
 
 source = sc.newAPIHadoopRDD(inputFormatClass="org.apache.hadoop.mapreduce.lib.input.CombineTextInputFormat",
                             keyClass="org.apache.hadoop.io.LongWritable",
