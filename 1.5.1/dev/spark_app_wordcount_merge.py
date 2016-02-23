2650edd94a47d0340f694fe24dff1a44bce5866c
yurun <miao.work2010@gmail.com>
2016-02-23 14:29:52 +0800

    dev

--- 1.5.1/dev/spark_app_wordcount_merge.py
+++ 1.5.1/dev/spark_app_wordcount_merge.py
@@ -7,14 +7,16 @@ conf.setAppName("spark_app_wordcount_merge")
 sc = SparkContext(conf=conf)
 
 hadoopConf = {"mapreduce.input.fileinputformat.inputdir": "/user/hdfs/rawlog/app_weibomobilekafka1234_topweiboimpression",
-              "mapreduce.input.fileinputformat.input.dir.recursive": "true"}
+              "mapreduce.input.fileinputformat.input.dir.recursive": "true",
+              "mapreduce.input.fileinputformat.split.minsize.per.node": "134217728",
+              "mapreduce.input.fileinputformat.split.minsize.per.rack": "268435456"}
 
 source = sc.newAPIHadoopRDD(inputFormatClass="org.apache.hadoop.mapreduce.lib.input.TextInputFormat",
                             keyClass="org.apache.hadoop.io.LongWritable",
                             valueClass="org.apache.hadoop.io.Text",
                             conf=hadoopConf)
 
-source = source.coalesce(1000)
+#source = source.coalesce(1000)
 
 lines = source.map(lambda pair: pair[1])
 
