ce8f6c297d7e7fb31a9a856c47b6b691d64b62c1
yurun <miao.work2010@gmail.com>
2016-03-15 18:43:34 +0800

    dev

--- 1.2.0/examples/app/spark_app_foreachPartition.py
+++ 1.2.0/examples/app/spark_app_foreachPartition.py
@@ -1,3 +1,4 @@
+import time
 from pyspark import SparkConf, SparkContext
 
 conf = SparkConf().setAppName("spark_app_foreachPartition")
@@ -7,8 +8,10 @@ sc = SparkContext(conf=conf)
 
 def log(iterator):
     for val in iterator:
+    	time.sleep(60)
+
         print val
 
-sc.parallelize([1, 2, 3, 4, 5]).coalesce(3).foreachPartition(log)
+sc.parallelize([1, 2, 3, 4, 5]).coalesce(1).foreachPartition(log)
 
 sc.stop()
