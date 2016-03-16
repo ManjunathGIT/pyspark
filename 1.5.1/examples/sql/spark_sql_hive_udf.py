45a369043539dc6a2859863b916f429f08bbb299
yurun <miao.work2010@gmail.com>
2016-03-16 13:48:38 +0800

    dev

--- 1.5.1/examples/sql/spark_sql_hive_udf.py
+++ 1.5.1/examples/sql/spark_sql_hive_udf.py
@@ -1,5 +1,5 @@
 from pyspark import SparkConf, SparkContext
-from pyspark.sql import HiveContext
+from pyspark.sql import HiveContext, Row
 
 conf = SparkConf().setAppName("spark_sql_hive_udf")
 
@@ -7,9 +7,19 @@ sc = SparkContext(conf=conf)
 
 hc = HiveContext(sc)
 
-rows = hc.sql("show functions").collect()
+lines = sc.parallelize(["a", "b", "c"])
+
+people = lines.map(lambda value: Row(name=value))
+
+peopleSchema = hc.inferSchema(people)
+
+peopleSchema.registerTempTable("people")
+
+#rows = hc.sql("select func.ipToLocationBySina('10.13.4.44') from people").collect()
+
+rows = hc.sql("SHOW FUNCTIONS like 'f.*'").collect()
 
 sc.stop()
 
 for row in rows:
-    print row
+    print row, type(row[0])
